3339ef98064795753c1d7a1c846920df28194a17
Jo Seunghyun <sky@izz.kr>
2015-09-05 17:27:55 +0900

    사용자명 및 메시지 파싱 기능 구현

--- tasks/base.rb
+++ tasks/base.rb
@@ -8,5 +8,14 @@ module Task
 
     def self.join(m)
     end
+
+    def self.parse(m)
+      user = m.user.nick
+      message = m.message
+      if m.user.name == "B"
+        user, message = message.match(/^<([^>]+)> (.*)$/)[1..2]
+      end
+      [user, message]
+    end
   end
 end
--- tasks/hello.rb
+++ tasks/hello.rb
@@ -3,8 +3,9 @@ require_relative 'base'
 module Task
   class HelloTask < BaseTask
     def self.message(m)
-      if m.message == "Hello"
-        m.reply "Hello, #{m.user.name}!"
+      user, message = parse(m)
+      if message == "Hello"
+        m.reply "Hello, #{user}!"
       end
     end
   end
